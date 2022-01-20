9322b874593d35d1ff306115b24369e0543de3d8
PurpleMongrel <thaispdf@gmail.com>
2022-01-20 17:41:31 -0500

    put request

--- PUT_request-test.js
+++ PUT_request-test.js
@@ -1,13 +1,21 @@
-const {request} = require("http");
+const { request } = require("http");
 let b;
 let r = request(
   "http://localhost:8000/home/thais/code/JavaScript/Eloquent_JavaScript/Chapter_20-Node/reverse.js",
   {
-  hostname: "localhost",
-  port: 8000,
-  method: "PUT",
-})
-r.write(JSON.stringify(r, null, 2))
+    hostname: "localhost",
+    port: 8000,
+    method: "PUT",
+  })
+  let arr = [];
+for (let prop in r) {
+  arr.push(prop);
+  arr.push(typeof r[prop]);
+  arr.push("***")
+
+}
+console.log(r.write.toString())
+r.write(JSON.stringify(arr))
 r.end();
 
 /* console.log("req:");
